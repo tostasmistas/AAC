d65e909dbe19e98294fe364b63ea7a2d0def4482
nmac93 <nuno.r.machado@ist.utl.pt>
2015-03-16 16:21:16 +0000

    memoriaRAMROM

--- uRisc_xilinx_1/memoria_ROM.vhd
+++ uRisc_xilinx_1/memoria_ROM.vhd
@@ -35,22 +35,29 @@ entity memoria_ROM is
 		);
 		Port(
 			CLK_A 	: in 	STD_LOGIC;
-			WE_A  	: in 	STD_LOGIC;
 			Addr_A 	: in 	STD_LOGIC_VECTOR(ADDR_SIZE-1 downto 0);
-			DI_A 	: in 	STD_LOGIC_VECTOR(31 downto 0);
-			DO_A 	: out 	STD_LOGIC_VECTOR(31 downto 0);
-			CLK_B   : in 	STD_LOGIC;
-			WE_B    : in 	STD_LOGIC;
-			Addr_B 	: in 	STD_LOGIC_VECTOR(ADDR_SIZE-1 downto 0);
-			DI_B  	: in 	STD_LOGIC_VECTOR(31 downto 0);
-			DO_B  	: out 	STD_LOGIC_VECTOR(31 downto 0)
+			DO_A 	: out 	STD_LOGIC_VECTOR(15 downto 0)
 		);
 end memoria_ROM;
 
 architecture Behavioral of memoria_ROM is
 
-begin
+type MEM_TYPE is array(0 to (2**ADDR_SIZE)-1) of STD_LOGIC_VECTOR(31 downto 0);
 
+constant InitValue : MEM_TYPE := ( 
+	others=> x"00000000"
+	);
+
+shared variable myRAM : MEM_TYPE := InitValue0;
+
+
+begin
+process (CLK_A)
+	begin
+		if rising_edge(CLK_A)then
+			DO_A <= RAM(conv_integer(Addr_A));
+	end if;
+end process;
 
 end Behavioral;
 
